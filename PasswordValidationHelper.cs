9b77123bb44898427987205ffd8e9fb4e40f94f3
Cozroth <t_ott_e@hotmail.com>
2023-09-18 14:29:55 +0200

    a

--- PasswordValidationHelper.cs
+++ PasswordValidationHelper.cs
@@ -1,102 +1,246 @@
 ﻿namespace ValidationUtility
 {
-    /// <summary>
-    /// Validation Helper Class for creating a Password.
-    /// </summary>
-    public class PasswordValidationHelper
+  /// <summary>
+  /// Validation Helper Class for creating a Password.
+  /// </summary>
+  public class PasswordValidationHelper
+  {
+    public static string PasswordValidation(
+            string prompt,
+            byte minLength = 8,
+            byte maxLength = 113,
+            bool mustHaveSpecialCharacters = true,
+            bool mustHaveNumbers = true,
+            bool mustHaveLowerAndUpperCaseLetters = true,
+            bool avoidIllegalChars = false,
+            char[]? specialChars = null,
+            char[]? illegalChars = null)
     {
-        public static string PasswordValidation(
-                string prompt,
-                byte minLength = 8,
-                byte maxLength = 113,
-                bool mustHaveSpecialCharacters = true,
-                bool mustHaveNumbers = true,
-                bool mustHaveLowerAndUpperCaseLetters = true,
-                bool avoidIllegalChars = false,
-                char[]? specialChars = null,
-                char[]? illegalChars = null)
-        {
-            specialChars ??= new char[] {
+      specialChars ??= new char[] {
             '@', '#', '$', '%', '^', '&','*', '-',
             '_', '!', '+', '=','[', ']', '{', '}',
             '|', '\\',':', '\'', ',', '.', '?', '/',
             '`', '~', '"', '(', ')', ';','<', '>' };
-            bool hasDigit, hasLower, hasUpper, hasSpecial, hasIllegal, emptyVal;
-            List<string> errorMessages;
-            while (true)
+      bool hasDigit, hasLower, hasUpper, hasSpecial, hasIllegal, emptyVal;
+      List<string> errorMessages;
+      while (true)
+      {
+        string userInput = CharValidationHelper.CharPasswordCreator(prompt, true);
+
+        hasDigit = false; hasLower = false; hasUpper = false; hasSpecial = false; hasIllegal = false;
+
+        errorMessages = new List<string>();
+        foreach (char c in userInput)
+        {
+          if (mustHaveNumbers && char.IsDigit(c))
+          {
+            hasDigit = true;
+          }
+
+          if (mustHaveLowerAndUpperCaseLetters)
+          {
+            if (char.IsLower(c))
             {
-                string userInput = CharValidationHelper.CharPasswordCreator(prompt, true);
-
-                hasDigit = false; hasLower = false; hasUpper = false; hasSpecial = false; hasIllegal = false;
-
-                errorMessages = new List<string>();
-                foreach (char c in userInput)
-                {
-                    if (mustHaveNumbers && char.IsDigit(c))
-                    {
-                        hasDigit = true;
-                    }
-
-                    if (mustHaveLowerAndUpperCaseLetters)
-                    {
-                        if (char.IsLower(c))
-                        {
-                            hasLower = true;
-                        }
-                        if (char.IsUpper(c))
-                        {
-                            hasUpper = true;
-                        }
-                    }
-
-                    if (mustHaveSpecialCharacters && specialChars.Contains(c))
-                    {
-                        hasSpecial = true;
-                    }
-
-                    if (avoidIllegalChars && illegalChars != null && illegalChars.Contains(c))
-                    {
-                        hasIllegal = true;
-                    }
-                }
-
-                if (userInput.Length < minLength || userInput.Length > maxLength)
-                {
-                    errorMessages.Add($"Password must be between {minLength} and {maxLength} characters.");
-                }
-
-                if (mustHaveNumbers && !hasDigit)
-                {
-                    errorMessages.Add("Password must contain at least one digit.");
-                }
-
-                if (mustHaveLowerAndUpperCaseLetters && (!hasLower || !hasUpper))
-                {
-                    errorMessages.Add("Password must contain both lower and upper case letters.");
-                }
-
-                if (mustHaveSpecialCharacters && !hasSpecial)
-                {
-                    errorMessages.Add("Password must contain at least one special character.");
-                }
-
-                if (avoidIllegalChars && hasIllegal)
-                {
-                    errorMessages.Add("Password contains illegal characters.");
-                }
-
-                // If there are error messages, display them and ask the user to recreate the password
-                if (errorMessages.Count > 0)
-                {
-                    ExceptionHelper.ExceptionDetails("\nPassword validation failed. Please fix the following issues:", errorMessages);
-                }
-                else
-                {
-                    return userInput;
-                }
+              hasLower = true;
             }
+            if (char.IsUpper(c))
+            {
+              hasUpper = true;
+            }
+          }
+
+          if (mustHaveSpecialCharacters && specialChars.Contains(c))
+          {
+            hasSpecial = true;
+          }
+
+          if (avoidIllegalChars && illegalChars != null && illegalChars.Contains(c))
+          {
+            hasIllegal = true;
+          }
         }
+
+        if (userInput.Length < minLength || userInput.Length > maxLength)
+        {
+          errorMessages.Add($"Password must be between {minLength} and {maxLength} characters.");
+        }
+
+        if (mustHaveNumbers && !hasDigit)
+        {
+          errorMessages.Add("Password must contain at least one digit.");
+        }
+
+        if (mustHaveLowerAndUpperCaseLetters && (!hasLower || !hasUpper))
+        {
+          errorMessages.Add("Password must contain both lower and upper case letters.");
+        }
+
+        if (mustHaveSpecialCharacters && !hasSpecial)
+        {
+          errorMessages.Add("Password must contain at least one special character.");
+        }
+
+        if (avoidIllegalChars && hasIllegal)
+        {
+          errorMessages.Add("Password contains illegal characters.");
+        }
+
+        // If there are error messages, display them and ask the user to recreate the password
+        if (errorMessages.Count > 0)
+        {
+          ExceptionHelper.ExceptionDetails("\nPassword validation failed. Please fix the following issues:", errorMessages);
+        }
+        else
+        {
+          return userInput;
+        }
+      }
     }
+
+    public static (List<string> ErrorMessages, string Password) SpectreConsolePasswordValidation(
+          string password,
+          byte minLength = 8,
+          byte maxLength = 113,
+          bool mustHaveSpecialCharacters = true,
+          bool mustHaveNumbers = true,
+          bool mustHaveLowerAndUpperCaseLetters = true,
+          bool avoidIllegalChars = false,
+          char[]? specialChars = null,
+          char[]? illegalChars = null)
+    {
+      specialChars ??= new char[] {
+            '@', '#', '$', '%', '^', '&','*', '-',
+            '_', '!', '+', '=','[', ']', '{', '}',
+            '|', '\\',':', '\'', ',', '.', '?', '/',
+            '`', '~', '"', '(', ')', ';','<', '>' };
+
+      bool hasDigit, hasLower, hasUpper, hasSpecial, hasIllegal, emptyVal;
+      List<string> errorMessages;
+      while (true)
+      {
+        hasDigit = false; hasLower = false; hasUpper = false; hasSpecial = false; hasIllegal = false;
+
+        errorMessages = new List<string>();
+        foreach (char c in password)
+        {
+          if (mustHaveNumbers && char.IsDigit(c))
+          {
+            hasDigit = true;
+          }
+
+          if (mustHaveLowerAndUpperCaseLetters)
+          {
+            if (char.IsLower(c))
+            {
+              hasLower = true;
+            }
+            if (char.IsUpper(c))
+            {
+              hasUpper = true;
+            }
+          }
+
+          if (mustHaveSpecialCharacters && specialChars.Contains(c))
+          {
+            hasSpecial = true;
+          }
+
+          if (avoidIllegalChars && illegalChars != null && illegalChars.Contains(c))
+          {
+            hasIllegal = true;
+          }
+        }
+
+        if (password.Length < minLength || password.Length > maxLength)
+        {
+          errorMessages.Add($"Password must be between {minLength} and {maxLength} characters.");
+        }
+
+        if (mustHaveNumbers && !hasDigit)
+        {
+          errorMessages.Add("Password must contain at least one digit.");
+        }
+
+        if (mustHaveLowerAndUpperCaseLetters && (!hasLower || !hasUpper))
+        {
+          errorMessages.Add("Password must contain both lower and upper case letters.");
+        }
+
+        if (mustHaveSpecialCharacters && !hasSpecial)
+        {
+          errorMessages.Add("Password must contain at least one special character.");
+        }
+
+        if (avoidIllegalChars && hasIllegal)
+        {
+          errorMessages.Add("Password contains illegal characters.");
+        }
+
+        return (ErrorMessages: errorMessages, Password: password.Trim());
+      }
+    }
+
+    public static (List<string> ErrorMessages, string Username) SpectreConsoleUsernameValidation(
+             string username,
+             byte minLength = 5,
+             byte maxLength = 113,
+             bool avoidWhiteSpaces = true,
+             bool avoidIllegalChars = true,
+             char[]? illegalChars = null)
+    {
+      illegalChars ??= new char[] {
+            '@', '#', '$', '%', '^', '&','*', '-',
+            '_', '!', '+', '=','[', ']', '{', '}',
+            '|', '\\',':', '\'', ',', '.', '?', '/',
+            '`', '~', '"', '(', ')', ';','<', '>' };
+
+      bool hasIllegal, hasWhiteSpace;
+      List<string> errorMessages;
+      while (true)
+      {
+        hasIllegal = false; hasWhiteSpace = false;
+        errorMessages = new List<string>();
+        foreach (char c in username)
+        {
+          if (avoidIllegalChars && illegalChars != null && illegalChars.Contains(c))
+          {
+            hasIllegal = true;
+          }
+
+          if (avoidWhiteSpaces)
+          {
+            if (c == ' ')
+            {
+              hasWhiteSpace = true;
+            }
+          }
+        }
+
+        if (avoidWhiteSpaces && hasWhiteSpace)
+        {
+          errorMessages.Add($"Username contains white spaces.");
+        }
+        if (username.Length < minLength || username.Length > maxLength)
+        {
+          errorMessages.Add($"Username must be between {minLength} and {maxLength} characters.");
+        }
+
+        if (avoidIllegalChars && hasIllegal)
+        {
+          errorMessages.Add("Username contains illegal characters.");
+        }
+        username = username.Normalize();
+        return (ErrorMessages: errorMessages, Username: username.Trim());
+      }
+    }
+  }
+
+
+
+
+
+
 }
+
 
 
--- StringValidationHelper.cs
+++ StringValidationHelper.cs
@@ -46,18 +46,7 @@ namespace ValidationUtility
         Console.WriteLine("Invalid input. You cannot use an empty text.");
       }
     }
-
-    //public static string GetCleanSpectreConsoleString(string input, string pattern = @"\[(.*?)\](.*?)\[/\]")
-    //{
-    //  Match match = Regex.Match(input, pattern);
-
-    //  string labelText = match.Groups[2].Value;
-    //  string cleanedLabel = string.Join("", labelText.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
-
-    //  return cleanedLabel;
-    //}
-
-    public static string GetCleanSpectreConsoleString(string input, string pattern = @"\[(.*?)\](.*?)\[/\]")
+    public static string GetCleanSpectreConsoleString(string input, bool avoidAllWhiteSpaces = true, bool avoidStartEndWhiteSpaces = true, string pattern = @"\[(.*?)\](.*?)\[/\]")
     {
       Regex regex = new Regex(pattern);
 
@@ -66,23 +55,92 @@ namespace ValidationUtility
 
         string content = match.Groups[2].Value;
 
-        string[] words = content.Trim().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-        StringBuilder transformedContent = new StringBuilder();
-
-        foreach (string word in words)
-        {
-          string cleanedWord = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(word.Replace(" ", "").Trim());
-
-          transformedContent.Append(cleanedWord);
-        }
-
-        return transformedContent.ToString();
+        return content;
       });
 
-      return result;
+      return CapitalizeWords(result, avoidAllWhiteSpaces, avoidStartEndWhiteSpaces);
+    }
+    public static string GetCleanSpectreConsoleStringForEnums(string input, bool avoidAllWhiteSpaces = true, bool avoidStartEndWhiteSpaces = true, string pattern = @"\[(.*?)\](.*?)\[/\]")
+    {
+      Regex regex = new Regex(pattern);
+
+      string result = regex.Replace(input, match =>
+      {
+
+        string content = match.Groups[2].Value;
+
+        return content;
+      });
+
+      return CapitalizeAndJoinWords(result, avoidAllWhiteSpaces, avoidStartEndWhiteSpaces);
     }
 
+    public static string CapitalizeFirstCharacter(string input, bool avoidAllWhiteSpaces = true, bool avoidStartEndWhiteSpaces = true)
+    {
+      string formattedInput = "";
+
+      if (string.IsNullOrEmpty(input))
+      {
+        return formattedInput;
+      }
+      if (avoidAllWhiteSpaces)
+      {
+        formattedInput = input.Replace(" ", "");
+      }
+      if (!avoidAllWhiteSpaces && avoidStartEndWhiteSpaces)
+      {
+        formattedInput = input.Trim();
+      }
+
+      formattedInput = formattedInput.First().ToString().ToUpper() + formattedInput.Substring(1).ToLower();
+
+      return formattedInput;
+    }
+    public static string CapitalizeAndJoinWords(string input, bool avoidAllWhiteSpaces = true, bool avoidStartEndWhiteSpaces = true)
+    {
+      string formattedInput = "";
+      List<string> inputWordList = new();
+      List<string> formattedWordsList = new();
+
+      if (string.IsNullOrEmpty(input))
+      {
+        return formattedInput;
+      }
+
+      inputWordList.AddRange(input.Split(' '));
+
+      foreach (string word in inputWordList)
+      {
+        formattedWordsList.Add(CapitalizeFirstCharacter(word, avoidAllWhiteSpaces, avoidStartEndWhiteSpaces));
+      }
+
+      formattedInput = string.Join("", formattedWordsList);
+
+      return formattedInput;
+    }
+
+    public static string CapitalizeWords(string input, bool avoidAllWhiteSpaces = true, bool avoidStartEndWhiteSpaces = true)
+    {
+      string formattedInput = "";
+      List<string> inputWordList = new();
+      List<string> formattedWordsList = new();
+
+      if (string.IsNullOrEmpty(input))
+      {
+        return formattedInput;
+      }
+
+      inputWordList.AddRange(input.Split(' '));
+
+      foreach (string word in inputWordList)
+      {
+        formattedWordsList.Add(CapitalizeFirstCharacter(word, avoidAllWhiteSpaces, avoidStartEndWhiteSpaces));
+      }
+
+      formattedInput = string.Join(" ", formattedWordsList);
+
+      return formattedInput;
+    }
     public static string CreateRandomString(int length = 60, string AllowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
     {
       Random Random = new Random();
