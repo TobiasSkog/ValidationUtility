8fb63a3a17c981a049babca8c8472b0fe2730183
Cozroth <t_ott_e@hotmail.com>
2023-09-18 14:34:28 +0200

    added username valdiation helping from using the spectreconsole

--- PasswordValidationHelper.cs
+++ PasswordValidationHelper.cs
@@ -180,66 +180,8 @@
       }
     }
 
-    public static (List<string> ErrorMessages, string Username) SpectreConsoleUsernameValidation(
-             string username,
-             byte minLength = 5,
-             byte maxLength = 113,
-             bool avoidWhiteSpaces = true,
-             bool avoidIllegalChars = true,
-             char[]? illegalChars = null)
-    {
-      illegalChars ??= new char[] {
-            '@', '#', '$', '%', '^', '&','*', '-',
-            '_', '!', '+', '=','[', ']', '{', '}',
-            '|', '\\',':', '\'', ',', '.', '?', '/',
-            '`', '~', '"', '(', ')', ';','<', '>' };
 
-      bool hasIllegal, hasWhiteSpace;
-      List<string> errorMessages;
-      while (true)
-      {
-        hasIllegal = false; hasWhiteSpace = false;
-        errorMessages = new List<string>();
-        foreach (char c in username)
-        {
-          if (avoidIllegalChars && illegalChars != null && illegalChars.Contains(c))
-          {
-            hasIllegal = true;
-          }
-
-          if (avoidWhiteSpaces)
-          {
-            if (c == ' ')
-            {
-              hasWhiteSpace = true;
-            }
-          }
-        }
-
-        if (avoidWhiteSpaces && hasWhiteSpace)
-        {
-          errorMessages.Add($"Username contains white spaces.");
-        }
-        if (username.Length < minLength || username.Length > maxLength)
-        {
-          errorMessages.Add($"Username must be between {minLength} and {maxLength} characters.");
-        }
-
-        if (avoidIllegalChars && hasIllegal)
-        {
-          errorMessages.Add("Username contains illegal characters.");
-        }
-        username = username.Normalize();
-        return (ErrorMessages: errorMessages, Username: username.Trim());
-      }
-    }
   }
-
-
-
-
-
-
 }
 
 
